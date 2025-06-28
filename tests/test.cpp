0c3fd91cea9d2f47b16bf25fb14b3a8e0c8cfb72
xfac11 <26203349+xfac11@users.noreply.github.com>
2025-06-28 21:59:37 +0200

    Added tests to the build using cmake and catch2. Now only using a test testing catch2. Next is to add tests for the game_engine.

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,15 @@
-#include<iostream>
-int main()
+#include <catch2/catch_test_macros.hpp>
+
+unsigned int Factorial(unsigned int number)
 {
-	std::cout << "Hello world\n";
-	return 0;
+	return number > 1 ? Factorial(number - 1) * number : 1;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" )
+{
+	REQUIRE( Factorial(0) == 1 );
+	REQUIRE( Factorial(1) == 1 );
+	REQUIRE( Factorial(2) == 2 );
+	REQUIRE( Factorial(3) == 6 );
+	REQUIRE( Factorial(10) == 3628800 );
 }
