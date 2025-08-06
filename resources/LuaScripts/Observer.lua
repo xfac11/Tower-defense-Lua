718645f7797edbed1ca0be4fae100e530985b07a
Filip Karlsson <xfac115@gmail.com>
2025-08-06 09:10:11 +0200

    TDWE-5 Added a function variable in Oberserver In observer class a new member variable called theFunction is added to be called by the update function which the subject uses when notifying observers.

--- resources/LuaScripts/Observer.lua
+++ resources/LuaScripts/Observer.lua
@@ -4,9 +4,16 @@ Observer.__index = Observer
 function Observer.new(name)
     local self = setmetatable({}, Observer)
     self.name = name or "Unnamed Observer"
+    self.theFunction = function(data)
+        print(self.name .. " received some data: " .. tostring(data))
+    end
     return self
 end
 
 function Observer:update(data)
-    print(self.name .. " received update: " .. tostring(data))
+    self.theFunction(data)
+end
+
+function Observer:setUpdateFunction(aFunction)
+    self.theFunction = aFunction
 end
